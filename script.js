b6a0b0cde65b09dfcfa52e0352e4620ddc6af1b1
shankstorm97 <work.prabhdeepsinngh@gmail.com>
2022-01-31 16:47:30 +0530

    my cat gen website

---
+++ script.js
@@ -0,0 +1,6 @@
+function catGenerator() {
+    var image = document.createElement('img');
+    var div = document.getElementById('cat-gen');
+    image.src = "/cat_gen/cat.jpeg";
+    div.appendChild(image);
+}
