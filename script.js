aa3bc31e1d2c7bdd08b5372b5355b423559c3fd3
shankstorm97 <work.prabhdeepsinngh@gmail.com>
2022-01-31 17:18:29 +0530

    arre bhai

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function catGenerator() {
     var image = document.createElement('img');
     var div = document.getElementById('cat-gen');
-    image.src = "/cat.jpeg";
+    image.src = "cat.jpeg";
     div.appendChild(image);
 }
